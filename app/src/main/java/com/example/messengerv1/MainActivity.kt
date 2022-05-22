a363103e54fe9fef1cde3a7c0e8bb8bba9b2d4ca
Шатилин Тимур Владимирович (4051834) <tshatilin@vtb.ru>
2022-05-22 16:58:22 +0300

    That's unbelievable!!!

--- app/src/main/java/com/example/messengerv1/MainActivity.kt
+++ app/src/main/java/com/example/messengerv1/MainActivity.kt
@@ -1,36 +1,40 @@
 package com.example.messengerv1
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val msgs = ArrayList<String>()
+        val dates = ArrayList<String>()
 
         val ButtonSendMsg: Button = findViewById(R.id.button_message_send)
         val RecyclerViewMsg: RecyclerView  = findViewById(R.id.recycler_view)
         val EditTextWriteMsg: EditText = findViewById(R.id.edit_text_message_write)
 
         RecyclerViewMsg.layoutManager = LinearLayoutManager(this)
-        var dataAdapter: DataAdapter = DataAdapter(msgs)
+        var dataAdapter: DataAdapter = DataAdapter(msgs,dates)
         RecyclerViewMsg.adapter = dataAdapter
 
         ButtonSendMsg.setOnClickListener {
             val message = EditTextWriteMsg.text.toString()
+            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = sdf.format(Date())
             msgs.add(message)
+            dates.add(currentDate)
             dataAdapter.notifyDataSetChanged()
             EditTextWriteMsg.setText("")
         }
-
-
     }
 }
